abcce14bfccd422c7fcbef47b455b26f9ed95020
Bruno Borsato <brunoborsato@outlook.com>
2018-10-20 15:39:57 -0300

    Postgres test setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+require( 'dotenv' ).config();
 const express = require( 'express' );
 const helmet = require( 'helmet' );
 const compression = require( 'compression' );
 const morgan = require( 'morgan' );
+const { Client } = require( 'pg' );
+
+const client = new Client( {
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+} );
+client.connect();
 
 const app = express();
-
 app.use( helmet() );
 app.use( compression() );
 app.use( express.json() );
@@ -16,4 +23,19 @@ app.get( '/', ( req, res ) => {
   } );
 } );
 
-app.listen( process.env.PORT || 3000 );
+app.get( '/pgtest', ( req, res ) => {
+  const sql = 'SELECT table_schema,table_name FROM information_schema.tables;';
+
+  client.query( sql, ( error, result ) => {
+    if ( error ) {
+      console.error( error );
+      res.status( 500 ).json( {
+        message: error.message,
+      } );
+    }
+
+    res.status( 200 ).json( result );
+  } );
+} );
+
+app.listen( process.env.PORT );
